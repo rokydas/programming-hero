5598c48306f82c5a0ba1f63fa03f200009b423a1
rokydas <droky136@gmail.com>
2022-12-08 03:05:44 +0600

    build issue fixed

--- pages/components/Factor.js
+++ pages/components/Factor.js
@@ -2,13 +2,16 @@ import Image from 'next/image';
 import React from 'react';
 
 const Factor = ({ style, img, factorData }) => {
+
+    const {cornerTitle, title, description} = factorData || {}
+
     return (
         <div className={`rounded-4`} style={style} >
             <div className="row" >
                 <div className="col-md-8 p-5">
-                    <p>{factorData.cornerTitle}</p>
-                    <h1>{factorData.title}</h1>
-                    <h4>{factorData.description}</h4>
+                    <p>{cornerTitle}</p>
+                    <h1>{title}</h1>
+                    <h4>{description}</h4>
                 </div>
                 <div className="col-md-4 p-5 bg-white rounded-end d-flex justify-content-center align-items-center">
                     <Image src={img} width="150" height="150" />
