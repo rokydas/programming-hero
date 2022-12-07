c59602bc994b0ffd75cb38aa3da89a2bbe519348
rokydas <droky136@gmail.com>
2022-12-08 02:59:23 +0600

    navbar modified

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,34 +1,39 @@
 import React from 'react';
+import styles from '../../styles/navbar.module.css'
+import logo from '../../images/ph_logo.png'
+import Image from 'next/image';
 
 const Navbar = () => {
     return (
-        <nav className={`my-nav navbar navbar-expand-lg bg-light my-nav`}>
+        <nav className={`${styles['nav-container']} navbar navbar-dark navbar-expand-lg`}>
             <div className="container-fluid">
-                <a className="navbar-brand ms-5 ps-5" href="#"></a>
+                <a className="navbar-brand ms-5 ps-5" href="#">
+                    <Image src={logo} width="40" height="40" />
+                </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul className="navbar-nav me-5 pe-5">
+                    <ul className={`navbar-nav ms-auto`}>
                         <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                            <a className="nav-link active" aria-current="page" href="#">Course details</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
+                            <a className="nav-link active" aria-current="page" href="#">About us</a>
                         </li>
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown link
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Login</a>
                         </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">5500</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#"><button className='btn btn-primary'>Enroll now</button></a>
+                        </li>
+                        
                     </ul>
                 </div>
             </div>
